230886909a9b0c078410bf72299a08f07238c4bb
AnVillab99 <34347348+AnVillab99@users.noreply.github.com>
2020-02-12 12:47:34 -0500

    pls heroku

--- src/main/java/edu/escuelaing/arep/reto1/webServer.java
+++ src/main/java/edu/escuelaing/arep/reto1/webServer.java
@@ -5,8 +5,10 @@ import java.io.BufferedReader;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -17,7 +19,7 @@ import java.awt.image.BufferedImage;
 
 public class webServer {
 
-    static final File ROOT = new File(System.getProperty("user.dir") + "/src/main/java/edu/escuelaing/arep/resources");
+    static final String ROOT = System.getProperty("user.dir") +"/src/main/java/edu/escuelaing/arep/resources";
     static final String DEFAULT = "/index.html";
     static final String FILE_NOT_FOUND = "/NOT_FOUND.html";
     static final String METHOD_NOT_ALLOWED = "/NOT_SUPPORTED.html";
@@ -34,6 +36,8 @@ public class webServer {
         PrintWriter out = null;
         BufferedReader in = null;
         BufferedOutputStream dataOut = null;
+        OutputStream outS =null;
+        
         Socket clientSocket = null;
         boolean conectado = true;
         while (conectado) {
@@ -46,6 +50,7 @@ public class webServer {
                     in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));// leer
                     out = new PrintWriter(clientSocket.getOutputStream(), true); // devolver
                     dataOut = new BufferedOutputStream(clientSocket.getOutputStream());
+                    outS = clientSocket.getOutputStream();
                     String inputLine = in.readLine();
                     String[] header = inputLine.split(" ");
                     /*while (inputLine != null) {
@@ -64,28 +69,37 @@ public class webServer {
                         File rFile = null;
                         if (header[1].equals(" ") ||header[1].equals("") ||header[1].equals("/")) {
                             rFile = new File(ROOT, DEFAULT);
-                            respond(out, dataOut, rFile, "text/html", "200 OK");
-                        } else {}
+                            respond(out, dataOut, rFile, "text/html", "200",ROOT+ DEFAULT,outS);
+                        
+                        } else {
                             String[] s = soportado(header[1]);
                             if (s[0].equals("ok")) {
-                                rFile = new File(ROOT, s[1] + header[1]);
+                                rFile = new File(ROOT+ s[1] + header[1]);
                                 if (rFile.exists()) {
-                                    respond(out, dataOut, rFile, s[2], "200 OK");
+                                    respond(out, dataOut, rFile, s[2], "200",ROOT+ s[1] + header[1],outS);
                                 } else {
                                     rFile = new File(ROOT, FILE_NOT_FOUND);
-                                    respond(out, dataOut, rFile, "text/html", "404");
+                                    respond(out, dataOut, rFile, "text/html", "404",ROOT+ FILE_NOT_FOUND,outS);
                                 }
                             }
                             else {
-                                System.out.println("deberia entrar por el favicon");
-                                rFile = new File(ROOT, UNSUPPORTED_MEDIA_TYPE);
-                                respond(out, dataOut, rFile, "text/html", "415");
+                                /*if(header[1].endsWith(".ico")){
+                                    System.out.println("favi");
+                                }
+                                else{*/
+                                    System.out.println("es el header "+header[1]);
+                                    rFile = new File(ROOT, UNSUPPORTED_MEDIA_TYPE);
+                                    respond(out, dataOut, rFile, "text/html", "415",ROOT+ UNSUPPORTED_MEDIA_TYPE,outS);
+                                //}
                             }
                         }
+                    }
                     
                     else {
-                        File f = new File(ROOT, METHOD_NOT_ALLOWED);
-                        respond(out, dataOut, f, "text/html", "405");
+                        
+                            File f = new File(ROOT, METHOD_NOT_ALLOWED);
+                            respond(out, dataOut, f, "text/html", "405",ROOT+METHOD_NOT_ALLOWED,outS);
+                        
                     }
                     out.close();
                     in.close();
@@ -151,41 +165,66 @@ public class webServer {
         return 4567; //returns default port if heroku-port isn't set
         }
 
-    private static void respond(PrintWriter out, BufferedOutputStream dataOut, File response, String type,
-            String code) {
-        out.println("HTTP/1.1 " + response);
-        out.println("Server: Java HTTP Server AnVillab99 : 1.0");
-        out.println("Date: " + new Date());
-        out.println("Content-type: " + type);
-        out.println("Content-length: " + response.length());
-        out.println();
-        out.flush();
+    private static void respond(PrintWriter out, BufferedOutputStream dataOut, File response, String type,String code, String filePath, OutputStream outS) {
+        String header = "HTTP/1.1 " + code+"\r\n"+
+        "Access-Control-Allow-Origin: *\r\n"+
+        "Content-type: " + type+"\r\n";
+        
         String[] con = type.split("/");
         try {
             if (con[0].equals("image")) {
-
+                System.out.println(type);
                 BufferedImage image = ImageIO.read(response);
                 ByteArrayOutputStream os = new ByteArrayOutputStream();
                 ImageIO.write(image, con[1], os);
+                byte[] r = os.toByteArray();
+                System.out.println("tamaño "+r.length);
+                header+= "Connection: close\r\n"+
+                "Content-length: " + r.length+"\r\n"+
+                "\r\n";
+                byte[] d = header.getBytes();
+                dataOut.write(d);
+                
+                
                 dataOut.write(os.toByteArray());
                 dataOut.flush();
+
+                System.out.println(os.toByteArray().length);
+                dataOut.close();
+
+
+
             } else {
-                byte[] fileByte = new byte[(int) response.length()];
-                FileInputStream fileO = null;
-                fileO = new FileInputStream(response);
-                fileO.read(fileByte);
-
-                if (fileO != null) {
-                    fileO.close();
+                header+="\r\n";
+                out.println(header);
+                BufferedReader reader = new BufferedReader(new FileReader(response));
+                StringBuffer result = new StringBuffer();
+                String res = "";
+                while ((res = reader.readLine()) != null) {
+                    result.append(res);
                 }
+                out.println(result);
+                // byte[] fileByte = new byte[(int) response.length()];
+                // FileInputStream fileO = null;
+                // fileO = new FileInputStream(response);
+                // fileO.read(fileByte);
 
-                dataOut.write(fileByte, 0, (int) response.length());
-                dataOut.flush();
+                // if (fileO != null) {
+                //     fileO.close();
+                // }
+                // out.println(fileByte);
+                out.flush();
+                out.close();
+                // dataOut.write(fileByte, 0, (int) response.length());
+                // dataOut.flush();
+                // dataOut.close();
             }
         } catch (Exception e) {
             System.out.println("erro en envio");
             System.out.println(e);
         }
+        out.flush();
+        out.close();
 
     }
 }
